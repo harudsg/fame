0f9475c2e0239d3a11f4ae5327f4135aaf333c1a
harudsg <168399002+harudsg@users.noreply.github.com>
2024-05-01 21:40:36 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Project information
 
