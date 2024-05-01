f49199d539ba99371bdac35d4b4ba03f9598499c
harudsg <168399002+harudsg@users.noreply.github.com>
2024-05-01 21:41:13 +0900

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -4,12 +4,6 @@ Lumache - Python library for cooks and food lovers.
 
 __version__ = "0.1.0"
 
-import os, sys
-import sphinx_rtd_theme
-
-pygments_stype = 'sphinx'
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
